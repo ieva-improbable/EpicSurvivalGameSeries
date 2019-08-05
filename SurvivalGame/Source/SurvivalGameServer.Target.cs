b4b5f0263cbaf128b457cd208a607b869172bc3b
Ieva Vasiljeva <ieva@improbable.io>
2019-08-05 16:43:07 +0100

    Adding server target for cloud builds.

---
+++ SurvivalGame/Source/SurvivalGameServer.Target.cs
@@ -0,0 +1,11 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class SurvivalGameServerTarget : TargetRules
+{
+	public SurvivalGameServerTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Server;
+		ExtraModuleNames.AddRange(new string[] { "SurvivalGame" });
+	}
+}
